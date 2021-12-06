186291452c1aad500bbf01dd88ae5d4f26634a36
Seokhun-Yang <ddayang1234@gmail.com>
2021-12-06 23:42:50 +0700

    initial commit

---
+++ src/Practice.java
@@ -0,0 +1,7 @@
+public class Practice {
+    public static void main(String[] args) {
+        int cost = 10000;
+        int paid = 50000;
+        System.out.println("Change: " + (paid - cost) + " Won");
+    }
+}
