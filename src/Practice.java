e1645b180e352640b9329f0b378b01bd050cf7a7
Seokhun-Yang <ddayang1234@gmail.com>
2021-12-06 23:49:30 +0700

    Usage of double

--- src/Practice.java
+++ src/Practice.java
@@ -3,5 +3,9 @@ public class Practice {
         int cost = 10000;
         int paid = 50000;
         System.out.println("Change: " + (paid - cost) + " Won");
+
+        double discount = 30; // 소수점 값을 나오게 하기 위해서는 int 가 아닌 double 을 사용한다.
+        int original = 43200;
+        System.out.println("Cost: " + (original * (discount / 100)));
     }
 }
